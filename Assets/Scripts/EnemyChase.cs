3db96f8b7e22de7ce57feb36881bf38b9f8dbcf5
aidane1023 <earnestaidan@gmail.com>
2024-02-18 09:19:22 -0500

    Enemy Aggro Range
    Enemy increases speed when close to player.

--- Assets/Scripts/EnemyChase.cs
+++ Assets/Scripts/EnemyChase.cs
@@ -7,9 +7,20 @@ public class EnemyChase : MonoBehaviour
     public UnityEngine.AI.NavMeshAgent creature;
     public UnityEngine.AI.NavMeshAgent player;
 
+    private float distance;
+
     void Update()
     {
-
         creature.SetDestination(player.transform.position);
+        distance = Vector3.Distance(creature.transform.position, player.transform.position);
+        //Circuit(); 
+        if(distance <= 10f)
+        {
+            creature.speed = 5f; 
+        }
+        else 
+        {
+            creature.speed = 2f;
+        }
     }
 }
