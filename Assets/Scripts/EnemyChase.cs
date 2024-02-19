afcb60732729018721651754ae94af8ddf628946
aidane1023 <earnestaidan@gmail.com>
2024-02-19 17:18:14 -0500

    Update EnemyChase.cs

--- Assets/Scripts/EnemyChase.cs
+++ Assets/Scripts/EnemyChase.cs
@@ -11,13 +11,45 @@ public class EnemyChase : MonoBehaviour
     private RaycastHit hit;
     public LayerMask mask;
 
+    private Vector3 direction = Vector3.forward;
+    private Vector3 axis = Vector3.up;
+    Vector3 rotation1;
+    Vector3 rotation2;
+
+
+
+    void Start()
+    {
+        Quaternion axisRotation = Quaternion.AngleAxis(15f, axis);
+        rotation1 = axisRotation * direction;
+
+        axisRotation = Quaternion.AngleAxis(-15f, axis);
+        rotation2 = axisRotation * direction;
+    }
+
 
     void Update()
     {
         // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, mask))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(direction), out hit, Mathf.Infinity, mask))
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.DrawRay(transform.position, transform.TransformDirection(direction) * hit.distance, Color.yellow);
+
+            this.GetComponent<EnemyPath>().enabled = false;
+            creature.SetDestination(player.transform.position);
+            creature.speed = 4f;
+        }
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(rotation1), out hit, Mathf.Infinity, mask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(rotation1) * hit.distance, Color.yellow);
+
+            this.GetComponent<EnemyPath>().enabled = false;
+            creature.SetDestination(player.transform.position);
+            creature.speed = 4f;
+        }
+        else if (Physics.Raycast(transform.position, transform.TransformDirection(rotation2), out hit, Mathf.Infinity, mask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(rotation2) * hit.distance, Color.yellow);
 
             this.GetComponent<EnemyPath>().enabled = false;
             creature.SetDestination(player.transform.position);
@@ -26,6 +58,8 @@ public class EnemyChase : MonoBehaviour
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.DrawRay(transform.position, transform.TransformDirection(rotation1) * 1000, Color.white);
+            Debug.DrawRay(transform.position, transform.TransformDirection(rotation2) * 1000, Color.white);
 
             this.GetComponent<EnemyPath>().enabled = true;
             creature.speed = 2f;
