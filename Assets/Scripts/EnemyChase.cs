c4c41f51756d78be15a227295c2dc5ba20ff279b
aidane1023 <earnestaidan@gmail.com>
2024-02-28 13:02:24 -0500

    Creature Animations
    Imported all creature animations and configured creature animation controller.

--- Assets/Scripts/EnemyChase.cs
+++ Assets/Scripts/EnemyChase.cs
@@ -13,62 +13,13 @@ public class EnemyChase : MonoBehaviour
         StartCoroutine(StallMovement());
     }
 
-    private Vector3 direction = Vector3.forward;
-    private Vector3 axis = Vector3.up;
-    Vector3 rotation1;
-    Vector3 rotation2;
-
-
-
-    void Start()
-    {
-        Quaternion axisRotation = Quaternion.AngleAxis(15f, axis);
-        rotation1 = axisRotation * direction;
-
-        axisRotation = Quaternion.AngleAxis(-15f, axis);
-        rotation2 = axisRotation * direction;
-    }
-
 
     void Update()
     {
-<<<<<<< Updated upstream
         Vector3 currentPosition = player.transform.position;
         creature.SetDestination(currentPosition);
     }
 
-=======
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(direction), out hit, Mathf.Infinity, mask))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(direction) * hit.distance, Color.yellow);
-
-            this.GetComponent<EnemyPath>().enabled = false;
-            creature.SetDestination(player.transform.position);
-            creature.speed = 4f;
-        }
-        else if (Physics.Raycast(transform.position, transform.TransformDirection(rotation1), out hit, Mathf.Infinity, mask))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(rotation1) * hit.distance, Color.yellow);
-
-            this.GetComponent<EnemyPath>().enabled = false;
-            creature.SetDestination(player.transform.position);
-            creature.speed = 4f;
-        }
-        else if (Physics.Raycast(transform.position, transform.TransformDirection(rotation2), out hit, Mathf.Infinity, mask))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(rotation2) * hit.distance, Color.yellow);
-
-            this.GetComponent<EnemyPath>().enabled = false;
-            creature.SetDestination(player.transform.position);
-            creature.speed = 4f;
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.DrawRay(transform.position, transform.TransformDirection(rotation1) * 1000, Color.white);
-            Debug.DrawRay(transform.position, transform.TransformDirection(rotation2) * 1000, Color.white);
->>>>>>> Stashed changes
 
     IEnumerator StallMovement()
     {
